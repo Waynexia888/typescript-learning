80ba0c42e81c7302e2df86bbd5a579c9a91ebb69
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 18:23:27 -0700

    updated

---
+++ basic-types.ts
@@ -0,0 +1,14 @@
+let isDone: boolean = false
+
+let age: number = 20
+let binaryNumber: number = 0b1111  // 二进制数字，前面+ 0b
+
+let fisrtName: string = 'wayne'
+let message: string = `Hello, ${fisrtName}, age is ${age}`
+
+let u: undefined = undefined   // undefined 是所有类型的子类型
+
+let n: null = null
+
+let num: number = undefined   // undefined 是所有类型的子类型
+
