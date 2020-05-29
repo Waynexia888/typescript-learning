a9c40be8dc2ba3281f6a820ee047598ba9ad38f2
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 21:16:19 -0700

    updated

--- generics.ts
+++ generics.ts
@@ -1,3 +1,4 @@
+// 泛型变量
 function echo<T>(arg: T): T {
     return arg
 }
@@ -11,3 +12,31 @@ function swap<T, U>(tuple: [T, U]): [U, T] {
 }
 
 const result2 = swap(['string', 123])
+
+//  约束泛型
+/////////////////////////////////////////////////
+function echoWithArr<T>(arg: T[]): T[] {
+    console.log(arg.length)
+    return arg
+}
+
+const arrs = echoWithArr([1, 2, 3])
+
+////////////////////////////////////////////////
+interface IWithLength {
+    length: number
+}
+
+function echoWithLength<T extends IWithLength>(arg: T): T {
+    console.log(arg.length)
+    return arg
+}
+
+const str = echoWithLength('str')
+const obj = echoWithLength({ length: 10, width: 10 })
+const arr2 = echoWithLength([1, 2, 3])
+// echoWithLength(13) // 报错， 因为13里面没有length这个属性
+
+///////////////////////////////////////////////////////////////
+
+// 类和接口
