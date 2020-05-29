b9d39c082bfd70ac0607ef385313f13fff1fd168
Waynexia888 <qiwuwaynexia@gmail.com>
2020-05-28 20:25:56 -0700

    updated

--- class.ts
+++ class.ts
@@ -1,5 +1,12 @@
 class Animal {
-    name: string;
+    public name: string;
+    // static 静态的， 不需要实例话，就可以访问一个类的静态方法和静态属性
+    static categories: string[] = ['mammal', 'bird']
+
+    static isAnimal(a) {
+        return a instanceof Animal
+    }
+
     constructor(name: string) {
         this.name = name
     }
@@ -8,8 +15,12 @@ class Animal {
     }
 }
 
+console.log(Animal.categories)
 const snake = new Animal('lily')
+console.log(Animal.isAnimal(snake))
 // console.log(snake.run())
+// snake.name = 'lucy'
+// console.log(snake.name)
 
 // 继承
 class Dog extends Animal {
